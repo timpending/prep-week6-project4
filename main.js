136be3c3bb0c2b76ef8cc37be690fdee3d4e0d67
Tim Pender <mrtimpender@gmail.com>
2016-11-16 16:00:04 -0700

    complete minus dropdown

---
+++ main.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+$('#submitBtn').on('click', function(){
+  $( "<p>" + $('#textInput').val() + "</p>" ).insertAfter( "#submitBtn" );
+})
+
+
+$('input[type=checkbox]').on('click', function(){
+  $('.jGod').toggleClass('hidden');
+})
+
+$('.table').append('<tr><td>Picnic</td><td>13</td><td>HUNGRY FOR SOULS</td><tr>')
+
+
+$('td').each(function(){
+  if (this.innerHTML== 'Hangry') {
+    this.style.color = 'red';
+  } else if (this.innerHTML== 'Content'){
+    this.style.color = 'green';
+  }
+})
+
+$('.heavy').on('click', function(){
+  $('td').each(function(){
+    let tempNum = Number(this.innerHTML)
+    if (tempNum < 10 ){
+      $(this).parent().hide();
+    }
+  })
+})
+
+$('.yell').on('click', function(){
+  $('td:nth-child(1)').each(function(){
+    let temp = this.innerHTML.toUpperCase()
+    this.innerHTML = temp
+  })
+})
+
+$('.hide-table').on('click', function(){
+  $('table').toggle();
+})
+
+
+$('#disableUs').on('click', function(e){
+  e.preventDefault();
+})
+
+// $('select.dropdown').on('click', function(){
+//   $('select.dropdown').find('option')[3];
+// })
+
+function countList() {
+  return $('#alertList').children().length
+}
+
+$('.count-list').on('click', function(){
+  alert('Total Amount in List: ' + countList())
+})
+
+function addToList(input) {
+  $('#alertList').append('<li>' + input + '</li>')
+}
+
+$('.add-list').on('click', function() {
+  let newAdd = prompt('What would you like to add');
+  addToList(newAdd)
+})
+
+$('.remove-list').on('click', function(){
+  $('#alertList li:last').remove();
+})
+
+
+
+});
